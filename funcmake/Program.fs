88db5173c0783686df811528729dc80f37e6ba59
Ahmed ElSayed <ahmels@microsoft.com>
2017-09-14 15:34:15 -0700

    Initial commit

---
+++ funcmake/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open MakeFunctionJson
+
+[<EntryPoint>]
+let main argv =
+    let result =
+        match argv.Length with
+        | 2 -> if FunctionJsonConvert.TryConvert (argv.[0], argv.[1]) then "Conversion was successful" else "There was an error"
+        | _ -> "USAGE: funcmake.exe <assemblyPath> <outputPath>"
+
+    printfn "%s" result
+    0
